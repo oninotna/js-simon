41845609a8d4a685319ab248b03043b3559bd67c
oninotna <antoninomusarratubbi@gmail.com>
2025-04-11 16:01:12 +0200

    print numbers generate on the html page

--- js/script.js
+++ js/script.js
@@ -2,12 +2,23 @@
 // FUNCTION
 // 
 function randomNumber (max, min = 1, totalNumber){
-    const arrayRandom = [];
+    let listNumber = "";
     for (i = 0; i < totalNumber; i++) {
         const currentRandomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-        arrayRandom.push(currentRandomNumber);
+        listNumber += `${currentRandomNumber} `;
     }
 
-    return arrayRandom;
+    return listNumber;
 }
 
+// #########################################################################
+// COUNTDOWN & NUMBERS 
+// 
+const numberGenerate = randomNumber(50, 1, 5);
+console.log(numberGenerate);
+
+// #########################################################################
+// INSERT ELEMENT IN DOCUMENT 
+//
+const numbersList = document.getElementById("numbers-list");
+numbersList.innerText = numberGenerate;
